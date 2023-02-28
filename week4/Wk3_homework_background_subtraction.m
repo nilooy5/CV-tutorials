3107d4c14e0faef14c31ab7c61ce724b9f00bfd3
Fazal Mahmud Niloy <niloooy@gmail.com>
2023-02-28 19:09:49 +1100

    week4 codes added.

---
+++ week4/Wk3_homework_background_subtraction.m
@@ -0,0 +1,55 @@
+% Background subtraction example on thermal images
+% Author: Roland Goecke, (c) 2018
+
+clear variables;    % This is similar to 'clear all' but more efficient.
+close all;
+clc;
+
+% Load images *** Change file path to your current settings ***
+backgroundImg = imread('Images\DINGO3_Background.jpeg');
+dingoImg = imread('Images\DINGO3_Frame0.jpeg');
+
+% Display images
+figure(1);
+imshow(backgroundImg);
+
+figure(2);
+imshow(dingoImg);
+
+% Compute difference image
+% (Literally, subtract the background image from the current image.)
+diffImg = dingoImg - backgroundImg;
+
+% Find the minimum pixel value. Divide by 255 as we will need this value
+% to be in the range of 0 to 1.
+minPixelValue = double(min(min(diffImg)))/255.0;
+disp('Min Pixel Value for Red, Green, Blue channels:');
+disp(minPixelValue);
+
+% Find the maximum pixel value. Divide by 255 as we will need this value
+% to be in the range of 0 to 1.
+maxPixelValue = double(max(max(diffImg)))/255.0;
+disp('Max Pixel Value for Red, Green, Blue channels:');
+disp(maxPixelValue);
+
+% Display difference image without rescaling
+figure(3);
+imshow(diffImg);
+
+% Display difference image with rescaling
+rescaledDiffImg = imadjust(diffImg, ...
+                    [minPixelValue(1) minPixelValue(2) minPixelValue(3); ...
+                     maxPixelValue(1) maxPixelValue(2) maxPixelValue(3)], ...
+                 []);
+figure(4);
+imshow(rescaledDiffImg);
+
+% Convert into greyscale image
+greyDiffImg = rgb2gray(rescaledDiffImg);
+figure(5);
+imshow(greyDiffImg);
+
+% Threshold image - Experiment with different threshold values!
+thresholdedImg = imbinarize(greyDiffImg, 0.1);
+figure(6);
+imshow(thresholdedImg);
