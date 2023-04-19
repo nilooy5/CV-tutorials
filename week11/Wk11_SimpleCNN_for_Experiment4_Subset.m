9aa786be202e949918e998340c6f440e3b94ee20
Fazal Mahmud Niloy <niloooy@gmail.com>
2023-04-19 19:06:10 +1000

    week 11 codes added

---
+++ week11/Wk11_SimpleCNN_for_Experiment4_Subset.m
@@ -0,0 +1,203 @@
+% Week 11 example code for training a simple CNN classifier on the
+% CUB_200_2011_Subset20classes dataset. Instead of using the full images,
+% we only use the bounding box area defined in bounding_boxes.txt.
+%
+% Authors: Roland Goecke and James Ireland. 
+% Date created: 02/05/2022
+% Date last updated: 17/04/23
+
+close all;
+clear variables;
+existing_GUIs = findall(0);
+if length(existing_GUIs) > 1
+    delete(existing_GUIs);
+end
+clc;
+
+%% Read the training, validation and test partitions from the relevant
+%  text files. 
+%  *** Adjust the file path as required. ***
+folder = "..\week9\CUB_200_2011_Subset20classes\";
+trainingImageNames = readtable(fullfile(folder, "train.txt"), ... 
+    'ReadVariableNames', false);
+trainingImageNames.Properties.VariableNames = {'index', 'imageName'};
+
+validationImageNames = readtable(folder + "validate.txt", ... 
+    'ReadVariableNames', false);
+validationImageNames.Properties.VariableNames = {'index', 'imageName'};
+
+testImageNames = readtable(folder + "test.txt", ... 
+    'ReadVariableNames', false);
+testImageNames.Properties.VariableNames = {'index', 'imageName'};
+
+%% Read class info from the relevant text files
+classNames = readtable(folder + "classes.txt", ...
+    'ReadVariableNames', false);
+classNames.Properties.VariableNames = {'index', 'className'};
+
+imageClassLabels = readtable(folder + "image_class_labels.txt", ...
+    'ReadVariableNames', false);
+imageClassLabels.Properties.VariableNames = {'index', 'classLabel'};
+
+%% Read bounding box information from bounding_boxes.txt. The format is
+%  image index, x-coordinate top-left corner, y-coordinate top-left corner,
+%  width, height.
+boundingBoxes = readtable(folder + "bounding_boxes.txt", ... 
+    'ReadVariableNames', false);
+boundingBoxes.Properties.VariableNames = {'index', 'x', 'y', 'w', 'h'};
+
+% Map bounding box information to the respective image file name
+train_image_box_map = returnMapping(trainingImageNames, boundingBoxes);
+val_image_box_map = returnMapping(validationImageNames, boundingBoxes);
+test_image_box_map = returnMapping(testImageNames, boundingBoxes);
+
+%% Create lists of image names for training, validation and test subsets.
+%  To be precise, we create an array of strings containing the full file
+%  path and file names for each data partition.
+trainingImageList = strings(height(trainingImageNames), 1);
+for iI = 1:height(trainingImageNames)
+    trainingImageList(iI) = string(fullfile(folder, "images/", ...
+        string(cell2mat(trainingImageNames.imageName(iI)))));
+end
+
+validationImageList = strings(height(validationImageNames), 1);
+for iI = 1:height(validationImageNames)
+    validationImageList(iI) = string(folder + "images/" + ...
+        string(cell2mat(validationImageNames.imageName(iI))));
+end
+
+testImageList = strings(height(testImageNames), 1);
+for iI = 1:height(testImageNames)
+    testImageList(iI) = string(folder + "images/" + ...
+        string(cell2mat(testImageNames.imageName(iI))));
+end
+
+%% Create image datastores for training, validation and test subsets
+trainingImageDS = imageDatastore(trainingImageList, 'labelSource', 'foldernames', ...
+    'FileExtensions', {'.jpg'});
+trainingImageDS.ReadFcn = @(filename) readImagesIntoDatastoreBB_Fast(filename, train_image_box_map);
+disp('Training set class samples:');
+countEachLabel(trainingImageDS)
+
+validationImageDS = imageDatastore(validationImageList, 'labelSource', 'foldernames', ...
+    'FileExtensions', {'.jpg'});
+validationImageDS.ReadFcn = @(filename) readImagesIntoDatastoreBB_Fast(filename, val_image_box_map);
+disp('Validation set class samples:');
+countEachLabel(validationImageDS)
+
+testImageDS = imageDatastore(testImageList, 'labelSource', 'foldernames',   'FileExtensions', {'.jpg'});
+testImageDS.ReadFcn = @(filename) readImagesIntoDatastoreBB_Fast(filename, test_image_box_map);
+disp('Test set class samples:');
+countEachLabel(testImageDS)
+
+%% The images all have different spatial resolutions (width x height), so 
+%  need to resize them to the same size. (Experiment with different sizes!)
+
+% Combine transformed datastores and labels 
+labelsTraining = arrayDatastore(trainingImageDS.Labels);
+labelsValidation = arrayDatastore(validationImageDS.Labels);
+labelsTest = arrayDatastore(testImageDS.Labels);
+
+cdsTraining = combine(trainingImageDS, labelsTraining);
+cdsValidation = combine(validationImageDS, labelsValidation);
+cdsTest = combine(testImageDS, labelsTest);
+
+% Resize all images to a common width and height
+targetSize = [224, 224];
+cdsTraining = transform(cdsTraining, @(x) preprocessData(x,targetSize));
+cdsValidation = transform(cdsValidation, @(x) preprocessData(x,targetSize));
+cdsTest = transform(cdsTest, @(x) preprocessData(x,targetSize));
+
+%% Display a sample image from the datastore
+figure(1);
+imshow(cdsTraining.read{1});
+title('Sample Image Resized');
+
+%% Create a simple CNN
+layers = [
+    imageInputLayer([224 224 3])    % This needs to match the image size chosen above
+    
+    convolution2dLayer(3, 8, 'Padding', 'same')
+    batchNormalizationLayer
+    reluLayer
+    maxPooling2dLayer(2, 'Stride', 2)
+    
+    convolution2dLayer(3, 16, 'Padding', 'same')
+    batchNormalizationLayer
+    reluLayer
+    maxPooling2dLayer(2, 'Stride', 2)
+    
+    convolution2dLayer(3, 32, 'Padding', 'same')
+    batchNormalizationLayer
+    reluLayer
+    maxPooling2dLayer(2, 'Stride', 2)
+    
+    fullyConnectedLayer(20)
+    softmaxLayer
+    classificationLayer];
+
+%% Check if we have a GPU available and clear any old data from it
+if (gpuDeviceCount() > 0)
+    device = gpuDevice(1);
+    reset(device);  % Clear previous values that might still be on the GPU
+end
+
+%% Set the training options
+options = trainingOptions('sgdm', ...
+        'InitialLearnRate', 0.001, ...
+        'MiniBatchSize', 20, ...
+        'MaxEpochs', 10, ...
+        'Verbose', true, ...
+        'Shuffle', 'every-epoch', ...
+        'VerboseFrequency', 1, ...
+        'ValidationData', cdsValidation, ...
+        'Plots','training-progress');
+
+%% Train the simple CNN model
+simpleCNN = trainNetwork(cdsTraining, layers, options);
+
+%% Test the accuracy on the test partition
+YPred = classify(simpleCNN, cdsTest);
+YTest = testImageDS.Labels;
+
+% Calculate overall accuracy
+accuracy = sum(YPred == YTest)/numel(YTest) % Output on command line
+
+% Show confusion matrix in figure
+[m, order] = confusionmat(YTest, YPred);
+figure(2);
+cm = confusionchart(m, order, ...
+    'ColumnSummary','column-normalized', ...
+    'RowSummary','row-normalized');
+title("Overall Accuracy: "+ string(round(accuracy*100, 1)) +"%");
+
+%% Compute classwise positive recogniton rate
+classwisePosRecog = zeros(height(order), 1);
+samplesPerRow = sum(m, 2);
+for iI = 1:height(order)
+    classwisePosRecog(iI) = round(100 * m(iI, iI) / samplesPerRow(iI), 1);
+end
+disp('Classwise Recognition Rates:');
+disp(classwisePosRecog)
+
+%% Helper function for resizing images in transform
+function data_out = preprocessData(data, targetSize)
+    try
+        data_out{1} = imresize(data{1}, targetSize(1:2)); % Resize images
+        data_out{2} = data{2};  % Keep labels as they are
+    catch e
+        % This is solely for debugging
+        disp(e) 
+    end
+end
+
+%% Helper function mapping image names to bounding boxes and vice versa
+function image_box_map = returnMapping(ImageNames, boundingBoxes)
+    image_box_map = containers.Map;
+    for i = 1:size(ImageNames, 1) 
+        fn = ImageNames{i,2}{1};
+        fn = split(fn, "\");
+        fn = split(fn, "/");
+        image_box_map(fn{end}) = [boundingBoxes{ImageNames{i,1}, 2:5}];
+    end
+end
